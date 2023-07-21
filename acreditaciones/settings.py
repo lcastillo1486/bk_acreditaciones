4eb2d212f7b3bcc42568644b875396fe5b83967d
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-07-21 14:17:09 -0500

    cambioBBDD

--- acreditaciones/settings.py
+++ acreditaciones/settings.py
@@ -86,13 +86,17 @@ WSGI_APPLICATION = 'acreditaciones.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+     'default': dj_database_url.config( default='postgresql://postgres:postgres@localhost:5432/mysite',conn_max_age=600    )
+        
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
