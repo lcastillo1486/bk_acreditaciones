a3c304818a35a4a146c6458541999d240dab2e1b
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-07-21 15:28:36 -0500

    pruebaimportar05

--- eventos/views.py
+++ eventos/views.py
@@ -116,7 +116,7 @@ def importarExcel(request, id_evento):
                 cargo=row['CARGO'],
                 zona_acceso=row['AREA_DE_TRABAJO'],
                 empresa=row['EMPRESA'],
-                id_evento_id=id_evento
+                # id_evento_id=id_evento
             )
             registros.append(registro)
 
