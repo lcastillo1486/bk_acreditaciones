217beb1ba7a03e45cabf181241f66c338431512f
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-07-23 19:06:28 -0500

    prueba3434

--- eventos/views.py
+++ eventos/views.py
@@ -321,9 +321,9 @@ def buscarPersona(request):
         
         #si busca empresa solamente
         if len(documento) == 0 and len(nombre) == 0 and len(apellido) == 0 and len(nombre_empresa) > 0:
-            if acreditados_def.objects.filter(empresa = nombre_empresa, id_evento_id = cod_event).exists():   
-                personal_empresa = acreditados_def.objects.filter(empresa = nombre_empresa, id_evento_id = cod_event)
-                return render(request,'personalempresa.html',{'personal':personal_empresa})
+            if acreditados_def.objects.filter(empresa__icontains = nombre_empresa, id_evento_id = cod_event).exists():   
+                # personal_empresa = acreditados_def.objects.filter(empresa = nombre_empresa, id_evento_id = cod_event)
+                return render(request,'personalempresa.html')
             else:
                 messages.error( request,'¡La empresa indicada no existe en los registros de este evento!')
                 total_acreditado = acreditados_def.objects.filter(id_evento_id = cod_event, acreditado = 1).count()
