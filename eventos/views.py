8249c509d3ec44ba5b51dceeea8f7e688e520e4a
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-08-16 15:50:33 -0500

    prueba98

--- eventos/views.py
+++ eventos/views.py
@@ -918,6 +918,14 @@ def exportarExcel(request, id):
         nueva_hoja.append([])  # Agregar una fila vacía como separador
         nueva_hoja.append(["Texto al final de la lista"])
         
+    hoja_totales = wb.create_sheet(title="Totales")
+
+    hoja_totales.append(['Empresa', 'Zona', 'Total Acreditados', 'Total No Acreditados'])
+    for (nombre_empresa, zona), empleados in empleados_por_empresa.items():
+        total_acreditados = sum(1 for empleado in empleados if empleado.status == 'Acreditado')
+        total_no_acreditados = sum(1 for empleado in empleados if empleado.status != 'Acreditado')
+
+    hoja_totales.append([nombre_empresa, zona, total_acreditados, total_no_acreditados])
 
     # Guardar el libro de Excel en la respuesta HTTP que lo mande el navegador
     wb.save(response)
