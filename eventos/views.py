0580d73134dc4c256b2e9cae7379482fc3f5091b
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-08-16 16:16:51 -0500

    prueba400

--- eventos/views.py
+++ eventos/views.py
@@ -924,13 +924,16 @@ def exportarExcel(request, id):
     hoja_totales = wb.create_sheet(title="Totales")
 
     queryset_total = acreditados_def.objects.filter(evento_cerrado=1, id_evento_id=id).order_by('apellido_persona')
+    hoja_totales.append(['Empresa', 'Zona', 'Total Acreditados', 'Total No Acreditados'])
     
     for (nombre_empresa, zona), empleados in empleados_por_empresa_zona.items():
         total_acreditados = sum(1 for empleado in empleados if empleado.status == 'Acreditado')
         total_no_acreditados = sum(1 for empleado in empleados if empleado.status != 'Acreditado')
 
+        hoja_totales.append([nombre_empresa, zona, total_acreditados, total_no_acreditados])
 
-    hoja_totales.append(['Empresa', 'Zona', 'Total Acreditados', 'Total No Acreditados'])
+
+    
     
 
     # Guardar el libro de Excel en la respuesta HTTP que lo mande el navegador
