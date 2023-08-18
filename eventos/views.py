f0ca351491db2e138e02191cbc2951e758f71daa
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2023-08-18 15:07:08 -0500

    correcion

--- eventos/views.py
+++ eventos/views.py
@@ -327,8 +327,12 @@ def buscarPersona(request):
 
 
     usuario = request.user
-    evento_buscar = acreditadorEvento.objects.get(usuario = usuario, cerrado = 0)
-    cod_event = evento_buscar.evento
+    if not acreditadorEvento.objects.filter(usuario = usuario, cerrado = 0).exists():
+        messages.error(request, '¡No se ha iniciado el proceso de acreditación!')
+        return redirect('evento')
+    else:
+        evento_buscar = acreditadorEvento.objects.get(usuario = usuario, cerrado = 0)
+        cod_event = evento_buscar.evento
 
 
         
