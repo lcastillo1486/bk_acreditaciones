d33e3b55c3822ffd0e063ec9ae9e5c1a0f45b379
lcastillo1486 <126116225+lcastillo1486@users.noreply.github.com>
2024-02-07 16:24:24 -0500

    debu

--- acreditaciones/settings.py
+++ acreditaciones/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 DATA_UPLOAD_MAX_NUMBER_FIELDS = 3000
 # Quick-start development settings - unsuitable for production
